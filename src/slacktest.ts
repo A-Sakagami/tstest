5c7491b47a608e7954597cab0fd743ab37e08891
Akiyasu Sakagami <akiyasu.sakagami@gmail.com>
2021-08-13 18:23:16 +0900

    test message bot create

--- src/slacktest.ts
+++ src/slacktest.ts
@@ -1,6 +1,16 @@
 import { WebClient } from '@slack/web-api';
 
-async() => {
-    const token = SLACK_BOT_TOKEN;
-};
+async function main(){
+    const token:string = 'xoxp-1977008656005-1973309065078-1980454774114-0ce080a1d523790373dbb6323044610b';
+    const channel:string = '#test';
+    const message:string = 'Hello, world! at TypeScript';
 
+    const client = new WebClient(token);
+    const params = {
+        channel: channel,
+        message: message
+    };
+    const resp = await client.chat.postMessage(params);
+    console.log(resp);
+}
+main();
