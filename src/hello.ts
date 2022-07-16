5f66b3c478f96e009f90992d74554b59123aa186
Akiyasu Sakagami <akiyasu.sakagami@gmail.com>
2022-07-17 01:31:50 +0900

    main

--- src/hello.ts
+++ src/hello.ts
@@ -7,8 +7,6 @@ const user = new User("taro","uamada",29); // 名前と年齢は適当に
 
 const contentsElem = document.getElementById('contents');
 
-
-
 if(!!contentsElem) {
     contentsElem.innerText = `${user.familyName} ${user.givenName}`;
 }
