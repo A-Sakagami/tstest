d0b5320c4d40efccaa345304fe015dc7bf245b86
Akiyasu Sakagami <akiyasu.sakagami@gmail.com>
2022-07-17 01:29:45 +0900

    commit

--- src/hello.ts
+++ src/hello.ts
@@ -3,9 +3,12 @@ function hello(){
 }
 hello();
 
-const user = new User('山田', '太郎', 29); // 名前と年齢は適当に
+const user = new User("taro","uamada",29); // 名前と年齢は適当に
 
 const contentsElem = document.getElementById('contents');
+
+
+
 if(!!contentsElem) {
     contentsElem.innerText = `${user.familyName} ${user.givenName}`;
 }
