d349a101b0e48954222bde7311458fb68f2cd14e
Akiyasu Sakagami <akiyasu.sakagami@gmail.com>
2022-07-19 00:47:43 +0900

    euc move test

--- src/euc.ts
+++ src/euc.ts
@@ -1,18 +1,20 @@
-const contentsNumber = document.getElementById('number');
+//const contentsNumber = document.getElementById('number');
 
 function euc(num1:number,num2:number) {
     let n = num1;
     let m = num2;
     if (n <= m) {
-        let exchange = n;
+        let exchange = m;
         n = m;
         m = exchange;
     }
     let mod : number;
-    while (n > 0) {
+    while (m > 0) {
         mod = n % m;
         n = m;
         m = mod;
     }
-    return alert(`${num1}と${num2}の最大公約数は${n}です。`);
+    return `${num1}と${num2}の最大公約数は${n}です。`;
 }
+
+console.log(euc(36,16));
