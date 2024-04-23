6ed6cc0621ebef6396575b6f6610ad20109f7a64
A-Sakagami <akiyasu.sakagami@gmail.com>
2024-04-24 00:50:51 +0900

    ロゴの追加、デザインの改善

--- src/dist/script.js
+++ src/dist/script.js
@@ -5,4 +5,13 @@ function selectSeat(seatId) {
     seat?.classList.toggle('selected');
     console.log(`Seat ${seatId} selected:`, seat?.classList.contains('selected'));
 }
+// 座席を予約する
+function reserveSeat(seatId, employeeId) {
+}
+// 座席の予約を取り消す
+function resetReserveSeat(seatIdSet, employeeId) {
+}
+// 座席の予約状況を確認する
+function searchReserveSeat(seatIdSet) {
+}
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -4,3 +4,18 @@ function selectSeat(seatId:string) {
     seat?.classList.toggle('selected');
     console.log(`Seat ${seatId} selected:`, seat?.classList.contains('selected'));
 }
+
+// 座席を予約する
+function reserveSeat(seatId:string, employeeId:string){
+
+}
+
+// 座席の予約を取り消す
+function resetReserveSeat(seatIdSet:string[], employeeId:string){
+
+}
+
+// 座席の予約状況を確認する
+function searchReserveSeat(seatIdSet:string[]){
+
+}
